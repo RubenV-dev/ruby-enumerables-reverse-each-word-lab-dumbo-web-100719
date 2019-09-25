2c6462db7ee65d9a1da97b70bbe7f16c904a392c
Ruben Vallejo <r.will16@hotmail.com>
2019-09-25 14:55:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ def reverse_each_word(string)
   string_array = string.split(" ")
   
   new_array = string_array.collect{|word| word.reverse}
-  p new_array
-  #reversed_string = string_array.join(" ")
-  #reversed_string
+  reversed_sentence = new_array.join(" ")
+  reversed_sentence
 end
