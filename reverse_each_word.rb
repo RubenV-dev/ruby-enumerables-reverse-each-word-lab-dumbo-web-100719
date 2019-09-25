daea31e0b44e20e816580dfc74c20778e12b2589
Ruben Vallejo <r.will16@hotmail.com>
2019-09-25 14:54:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   string_array = string.split(" ")
   
-  string_array.collect{|word| word.reverse}
-  p string_array
+  new_array = string_array.collect{|word| word.reverse}
+  p new_array
   #reversed_string = string_array.join(" ")
   #reversed_string
 end
